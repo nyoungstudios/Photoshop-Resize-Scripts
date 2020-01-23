b841f047ce2e91bf3491552862597cece98bc0d6
Nathaniel Young <nathanielyoung95070@gmail.com>
2020-01-23 03:34:59 -0500

    adds resize for single file

---
+++ resizeFileForSocialMedia.jsx
@@ -0,0 +1,116 @@
+// Created by Nathaniel Young (nyoungstudios)
+// this script resizes a photo with the optimal settings for social media such as Facebook or Instagram
+// saves resized files in folder that the user selects
+
+//wrapper function
+function main() {
+  
+  // opens file dialog for user to select the file to process
+  var filePathName = File.openDialog("Pick a file");
+  
+  // checks if the user does not select a file
+  if (filePathName == null) {
+    return;
+  } else {
+    filePathName = filePathName.toString();
+  }
+
+  // opens folder dialog for user to select the folder to process
+  var saveLocation = Folder.selectDialog("Pick a destination folder");
+
+  // checks if the user does not select a folder
+  if (saveLocation == null) {
+    return;
+  } else {
+    saveLocation = saveLocation.toString() + '/';
+  }
+
+  // parameters
+  var widthOfDocument = 2048;
+  var quality = 12;
+
+
+  // removes the file extension
+  var baseDocName = filePathName.substring(filePathName.lastIndexOf('/') + 1, filePathName.lastIndexOf('.'));
+
+
+  // appends underscore (_) if the original document name ends in a number
+  var lastChar = baseDocName[baseDocName.length - 1];
+
+  if (lastChar >= '0' && lastChar <= '9') {
+    baseDocName += '_';
+  }
+
+  // appends the width of the document
+  baseDocName += widthOfDocument;
+
+
+  // =======================================================
+  // opens photo
+  var idOpn = charIDToTypeID( "Opn " );
+      var desc01 = new ActionDescriptor();
+      var iddontRecord = stringIDToTypeID( "dontRecord" );
+      desc01.putBoolean( iddontRecord, false );
+      var idforceNotify = stringIDToTypeID( "forceNotify" );
+      desc01.putBoolean( idforceNotify, true );
+      var idnull = charIDToTypeID( "null" );
+      desc01.putPath( idnull, new File( filePathName ) );
+      var idDocI = charIDToTypeID( "DocI" );
+      desc01.putInteger( idDocI, 286 );
+  executeAction( idOpn, desc01, DialogModes.NO );
+
+  // =======================================================
+  // gets the current active document open
+  currentDocument = app.activeDocument;
+
+  // =======================================================
+  // resizes photo
+  var idImgS = charIDToTypeID( "ImgS" );
+      var desc02 = new ActionDescriptor();
+      var idWdth = charIDToTypeID( "Wdth" );
+      var idPxl = charIDToTypeID( "#Pxl" );
+      desc02.putUnitDouble( idWdth, idPxl, widthOfDocument );
+      var idscaleStyles = stringIDToTypeID( "scaleStyles" );
+      desc02.putBoolean( idscaleStyles, true );
+      var idCnsP = charIDToTypeID( "CnsP" );
+      desc02.putBoolean( idCnsP, true );
+      var idIntr = charIDToTypeID( "Intr" );
+      var idIntp = charIDToTypeID( "Intp" );
+      var idautomaticInterpolation = stringIDToTypeID( "automaticInterpolation" );
+      desc02.putEnumerated( idIntr, idIntp, idautomaticInterpolation );
+  executeAction( idImgS, desc02, DialogModes.NO );
+
+  // =======================================================
+  // saves jpg photo
+  var idsave = charIDToTypeID( "save" );
+      var desc03 = new ActionDescriptor();
+      var idAs = charIDToTypeID( "As  " );
+          var desc04 = new ActionDescriptor();
+          var idEQlt = charIDToTypeID( "EQlt" );
+          desc04.putInteger( idEQlt, quality );
+          var idMttC = charIDToTypeID( "MttC" );
+          var idMttC = charIDToTypeID( "MttC" );
+          var idNone = charIDToTypeID( "None" );
+          desc04.putEnumerated( idMttC, idMttC, idNone );
+      var idJPEG = charIDToTypeID( "JPEG" );
+      desc03.putObject( idAs, idJPEG, desc04 );
+      var idIn = charIDToTypeID( "In  " );
+      desc03.putPath( idIn, new File( saveLocation + baseDocName + ".jpg" ) );
+      var idDocI = charIDToTypeID( "DocI" );
+      desc03.putInteger( idDocI, 1363 );
+      var idCpy = charIDToTypeID( "Cpy " );
+      desc03.putBoolean( idCpy, true );
+      var idsaveStage = stringIDToTypeID( "saveStage" );
+      var idsaveStageType = stringIDToTypeID( "saveStageType" );
+      var idsaveBegin = stringIDToTypeID( "saveBegin" );
+      desc03.putEnumerated( idsaveStage, idsaveStageType, idsaveBegin );
+  executeAction( idsave, desc03, DialogModes.NO );
+
+  // =======================================================
+  // closes photoshop file
+  currentDocument.close(SaveOptions.DONOTSAVECHANGES);
+
+
+};
+
+main();
---
+++ resizeFileForWeb.jsx
@@ -0,0 +1,139 @@
+// Created by Nathaniel Young (nyoungstudios)
+// this script resizes a photo with the optimal settings for web use including services like Squarespace or Wix
+// saves resized files in folder that the user selects
+
+//wrapper function
+function main() {
+
+  // opens file dialog for user to select the file to process
+  var filePathName = File.openDialog("Pick a file");
+  
+  // checks if the user does not select a file
+  if (filePathName == null) {
+    return;
+  } else {
+    filePathName = filePathName.toString();
+  }
+
+  // opens folder dialog for user to select the folder to process
+  var saveLocation = Folder.selectDialog("Pick a destination folder");
+  
+  // checks if the user does not select a folder
+  if (saveLocation == null) {
+    return;
+  } else {
+    saveLocation = saveLocation.toString() + '/';
+  }
+
+  // parameters
+  var longEdgeOfDocument = 2048;
+  var quality = 10;
+
+
+  // removes the file extension
+  var baseDocName = filePathName.substring(filePathName.lastIndexOf('/') + 1, filePathName.lastIndexOf('.'));
+
+  
+  // appends underscore (_) if the original document name ends in a number
+  var lastChar = baseDocName[baseDocName.length - 1];
+
+  if (lastChar >= '0' && lastChar <= '9') {
+    baseDocName += '_';
+  }
+
+
+  // =======================================================
+  // opens photo
+  var idOpn = charIDToTypeID( "Opn " );
+      var desc01 = new ActionDescriptor();
+      var iddontRecord = stringIDToTypeID( "dontRecord" );
+      desc01.putBoolean( iddontRecord, false );
+      var idforceNotify = stringIDToTypeID( "forceNotify" );
+      desc01.putBoolean( idforceNotify, true );
+      var idnull = charIDToTypeID( "null" );
+      desc01.putPath( idnull, new File( filePathName ) );
+      var idDocI = charIDToTypeID( "DocI" );
+      desc01.putInteger( idDocI, 286 );
+  executeAction( idOpn, desc01, DialogModes.NO );
+
+  // =======================================================
+  // gets the current active document open
+  currentDocument = app.activeDocument;
+  var originalWidthOfDocument = parseInt(currentDocument.width.toString().replace(' px', ''));
+  var originalHeightOfDocument = parseInt(currentDocument.height.toString().replace(' px', ''));
+
+
+  if (originalWidthOfDocument >= originalHeightOfDocument) {
+    // =======================================================
+    // resizes photo if it is in landsacpe orientation
+    var idImgS = charIDToTypeID( "ImgS" );
+        var desc02 = new ActionDescriptor();
+        var idWdth = charIDToTypeID( "Wdth" );
+        var idPxl = charIDToTypeID( "#Pxl" );
+        desc02.putUnitDouble( idWdth, idPxl, longEdgeOfDocument );
+        var idscaleStyles = stringIDToTypeID( "scaleStyles" );
+        desc02.putBoolean( idscaleStyles, true );
+        var idCnsP = charIDToTypeID( "CnsP" );
+        desc02.putBoolean( idCnsP, true );
+        var idIntr = charIDToTypeID( "Intr" );
+        var idIntp = charIDToTypeID( "Intp" );
+        var idautomaticInterpolation = stringIDToTypeID( "automaticInterpolation" );
+        desc02.putEnumerated( idIntr, idIntp, idautomaticInterpolation );
+    executeAction( idImgS, desc02, DialogModes.NO );
+
+  } else {
+    // =======================================================
+      // resizes photo if it is in portrait orientation
+    var idImgS = charIDToTypeID( "ImgS" );
+        var desc05 = new ActionDescriptor();
+        var idHght = charIDToTypeID( "Hght" );
+        var idPxl = charIDToTypeID( "#Pxl" );
+        desc05.putUnitDouble( idHght, idPxl, longEdgeOfDocument );
+        var idscaleStyles = stringIDToTypeID( "scaleStyles" );
+        desc05.putBoolean( idscaleStyles, true );
+        var idCnsP = charIDToTypeID( "CnsP" );
+        desc05.putBoolean( idCnsP, true );
+        var idIntr = charIDToTypeID( "Intr" );
+        var idIntp = charIDToTypeID( "Intp" );
+        var idautomaticInterpolation = stringIDToTypeID( "automaticInterpolation" );
+        desc05.putEnumerated( idIntr, idIntp, idautomaticInterpolation );
+    executeAction( idImgS, desc05, DialogModes.NO );
+  }
+
+  // appends the width of the document
+  baseDocName += currentDocument.width.toString().replace(' px', '');
+
+  // =======================================================
+  // saves jpg photo
+  var idsave = charIDToTypeID( "save" );
+      var desc03 = new ActionDescriptor();
+      var idAs = charIDToTypeID( "As  " );
+          var desc04 = new ActionDescriptor();
+          var idEQlt = charIDToTypeID( "EQlt" );
+          desc04.putInteger( idEQlt, quality );
+          var idMttC = charIDToTypeID( "MttC" );
+          var idMttC = charIDToTypeID( "MttC" );
+          var idNone = charIDToTypeID( "None" );
+          desc04.putEnumerated( idMttC, idMttC, idNone );
+      var idJPEG = charIDToTypeID( "JPEG" );
+      desc03.putObject( idAs, idJPEG, desc04 );
+      var idIn = charIDToTypeID( "In  " );
+      desc03.putPath( idIn, new File( saveLocation + baseDocName + ".jpg" ) );
+      var idDocI = charIDToTypeID( "DocI" );
+      desc03.putInteger( idDocI, 1363 );
+      var idCpy = charIDToTypeID( "Cpy " );
+      desc03.putBoolean( idCpy, true );
+      var idsaveStage = stringIDToTypeID( "saveStage" );
+      var idsaveStageType = stringIDToTypeID( "saveStageType" );
+      var idsaveBegin = stringIDToTypeID( "saveBegin" );
+      desc03.putEnumerated( idsaveStage, idsaveStageType, idsaveBegin );
+  executeAction( idsave, desc03, DialogModes.NO );
+
+  // =======================================================
+  // closes photoshop file
+  currentDocument.close(SaveOptions.DONOTSAVECHANGES);
+
+
+};
+
+main();
